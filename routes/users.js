13cf27e5697fce6e68c29db292b413657f77a8bf
Fumitoshi Ogata <ogata.fumitoshi@o-12984-mac.local>
2019-10-25 13:46:22 +0900

    update

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+var _json = ''
+    _json += '{';
+    _json += '}';
+router.get('/test', function (req, res) {
+	res.json(JSON.parse(_json))
+});
+
+
 module.exports = router;
